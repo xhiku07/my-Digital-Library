7f47080685347cefd17951759def838d8032088b
Mithilesh Dhok <chikudhok@gmail.com>
2026-05-28 16:10:11 +0530

    updated database.py

--- database.py
+++ database.py
@@ -1,9 +1,13 @@
 import sqlite3
+import os
+
+DB_PATH = '/data/library.db' if os.path.exists('/data') else 'library.db'
 
 def get_db():
-    conn = sqlite3.connect('library.db')
+    conn = sqlite3.connect(DB_PATH)
     conn.row_factory = sqlite3.Row
     return conn
+
 
 def init_db():
     conn = get_db()
